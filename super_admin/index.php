7e48425ceb7bf1a93b0d8b105782ddf50bc1548e
mattIwago <daligdig.manuel19@gmail.com>
2025-03-24 23:46:53 +0800

    Update index.php

--- super_admin/index.php
+++ super_admin/index.php
@@ -1,9 +1,21 @@
 <?php
 require_once '../include/config.php';
 require_once '../include/session.php';
-echo isset($_SESSION["user_id"]) ? $users_id = $_SESSION["user_id"] : header("location: ../index.php");
+echo isset($_SESSION["user_id"]) ? $users_id = $_SESSION["user_id"] : "no login session"; /* you can use here the header('location: ../index.php') */
 
 ?>
 
 
-<a href="../logout.php"><button>LOGOUT</button></a>
+
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
