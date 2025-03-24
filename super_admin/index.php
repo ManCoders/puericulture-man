841c000346894f8f908349822f0d04ef767a78cb
mattIwago <daligdig.manuel19@gmail.com>
2025-03-24 23:32:34 +0800

    Update_login
    hahaha

--- super_admin/index.php
+++ super_admin/index.php
@@ -3,8 +3,9 @@
 require_once '../include/config.php';
 require_once '../include/session.php';
 
-echo isset($_SESSION["user_id"]) ? $users_id = $_SESSION["user_id"] : "no user_id";
+echo isset($_SESSION["user_id"]) ? $users_id = $_SESSION["user_id"] : header("location: ../index.php");
 
 ?>
 
 
+<a href="../logout.php"><button>LOGOUT</button></a>
